ef6fdcafe19e795e413d2268e105e1bc3c571926
Samriddhi Puranik <samriddhi.18puranik@gmail.com>
2024-01-09 09:34:59 +0530

    done

---
+++ process.php
@@ -0,0 +1,25 @@
+<?php
+
+// connecting to database
+$conn = mysqli_connect("localhost","root","","ajaxtest")
+;
+echo 'Processing ...';
+
+
+// check for post variable
+if(isset($_POST['name'])){
+    $name = mysqli_real_escape_string($conn,$_POST['name']);
+    // echo 'POST: Your name is '. $_POST['name'];
+    $query = "INSERT INTO users(name) VALUES('$name')";
+    if(mysqli_query($conn,$query)){
+        echo 'User Added...';
+
+    }else{
+        echo 'ERROR: '. mysqli_error($conn);
+    }
+}
+
+// check for get variable
+if(isset($_GET['name'])){
+    echo 'GET: Your name is '. $_GET['name'];
+}
